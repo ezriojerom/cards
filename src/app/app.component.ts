16c6ec70bbb4617e3863b11878e55494278357b3
joelalive <joelimalive1994@gmail.com>
2021-12-08 12:53:51 +0530

    Hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts=[ {
+    title: 'Neat Tree',
+    imageUrl: 'assets/tree.jpeg',
+    username: 'nature',
+    content: 'I saw this neat tree today'
+  },
+  {
+    title: 'Snowy Mountain',
+    imageUrl: 'assets/mountain.jpeg',
+    username: 'mountain1222',
+    content: 'I saw this neat tree today'
+  },
+  {
+    title: 'Mountain Biking',
+    imageUrl: 'assets/biking.jpeg',
+    username: 'biking1222',
+    content: 'I saw this neat tree today'
+  }];
 }
